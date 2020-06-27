814a7f959da60104771a39b7ffdc028df561a731
Sumudu Lakruwan <lakruwanpathirage01@gmail.com>
2020-06-27 10:32:32 +0530

    add a add function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,13 @@
-alert('This works!');
+const defaultResult= 0;
+let CurrentResult= defaultResult;
+
+function add(num1, num2){
+    const result= num1 + num2;
+    return result;
+}
+
+CurrentResult= add(1, 2);
+
+
+let calculationDescription='(' + defaultResult + '+ 10) *3/2-1';
+outputResult(CurrentResult, calculationDescription);
